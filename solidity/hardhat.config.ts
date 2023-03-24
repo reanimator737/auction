26868168041c6f1246042340f3ddb5119acddc39
Alexandr Kairtsev <reanimator737@gmail.com>
2023-03-25 01:30:40 +0200

    start front

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -2,19 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-coverage";
 
-const ALCHEMY_API_KEY = 'Q1VjahmEI8-uW5zwHCn48yIqKGBELNmi'
-
-//Don't steal my wallet
-const SEPOLIA_PRIVATE_KEY = 'a54ff2770d0d717b599ba93e65c00393df3a600a3494e91ae2920efae53ea3d3'
-
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-  networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY ]
-    }
-  }
 };
 
 export default config;
